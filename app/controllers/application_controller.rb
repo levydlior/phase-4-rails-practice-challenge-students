c55f2ca2bf5bf67c3a967fbf68ae411cfefab5c5
Lior <levydlior@gmail.com>
2022-07-21 10:46:41 -0600

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,15 @@
 class ApplicationController < ActionController::API
+rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+rescue_from ActiveRecord::RecordInvalid, with: :render_invalid
+
+    private
+
+    def render_not_found(exception)
+        render json: {errors: "#{exception.model} not found"}, status: :not_found
+    end
+
+    def render_invalid(invalid)
+        render json: {errors: invalid.record.errors.full_messages}, status: :unprocessable_entity
+    end
+
 end
